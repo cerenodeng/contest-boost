5f8f44b5781a0a6525824a874cdd01999fca841a
Cereno Deng <cereno.deng@outlook.com>
2023-06-01 23:25:19 +0800

    add content of animated tabs

--- app/components/AnimatedTabs.js
+++ app/components/AnimatedTabs.js
@@ -4,37 +4,45 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 let tabs = [
-  { id: "goals", label: "Goals" },
-  { id: "rules", label: "Rules" },
-  { id: "prizes", label: "Prizes" },
+  { id: "goals", label: "Goals", content: "The content of goals" },
+  { id: "rules", label: "Rules", content: "The content of rules" },
+  { id: "prizes", label: "Prizes", content: "The content of prizes" },
 ];
 
 export default function AnimatedTabs() {
   let [activeTab, setActiveTab] = useState(tabs[0].id);
 
   return (
-    <div className="flex gap-x-10">
+    <div className="flex flex-col gap-y-5">
+      <div className="flex gap-x-10">
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`${activeTab === tab.id ? "text-white" : "text-gray-900 hover:text-gray-900/60"} relative rounded px-7 py-2 text-base font-medium outline-sky-400 transition focus-visible:outline-2`}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {activeTab === tab.id && (
+              <motion.span
+                layoutId="bubble"
+                className="absolute inset-0 z-10 bg-black mix-blend-difference"
+                style={{ borderRadius: 4 }}
+                transition={{ type: "spring", bounce: 0.2, duration: 0.7 }}
+              />
+            )}
+            {tab.label}
+          </button>
+        ))}
+      </div>
+
       {tabs.map((tab) => (
-        <button
-          key={tab.id}
-          onClick={() => setActiveTab(tab.id)}
-          className={`${activeTab === tab.id ? "text-white" : "text-gray-900 hover:text-gray-900/60"
-            } relative rounded px-7 py-2 text-base font-medium outline-sky-400 transition focus-visible:outline-2`}
-          style={{
-            WebkitTapHighlightColor: "transparent",
-          }}
-        >
-          {activeTab === tab.id && (
-            <motion.span
-              layoutId="bubble"
-              className="absolute inset-0 z-10 bg-black mix-blend-difference"
-              style={{ borderRadius: 4 }}
-              transition={{ type: "spring", bounce: 0.2, duration: 0.7 }}
-            />
-          )}
-          {tab.label}
-        </button>
+        <div key={tab.id} className={`${activeTab === tab.id ? "" : "hidden"}`}>
+          {tab.content}
+        </div>
       ))}
     </div>
+
   );
 }
--- app/contests/[slug]/page.js
+++ app/contests/[slug]/page.js
@@ -8,26 +8,6 @@ export default function Page({ params }) {
     <div className="flex flex-col gap-y-10 w-full items-center">
       <AnimatedTabs />
 
-      <div className="flex gap-x-20 border-b border-gray-300 mx-20">
-        <div className="relative w-full h-96">
-          <label htmlFor="goals" className="flex justify-center cursor-pointer p-3 hover:bg-gray-200">Goals</label>
-          <input id="goals" className="hidden peer/goals" type="radio" name="nav" defaultChecked />
-          <div className="invisible absolute inset-x-0 bottom-0 top-20 bg-white peer-checked/goals:visible">the content of goals</div>
-        </div>
-
-        <div className="relative w-full h-96">
-          <label htmlFor="rules" className="flex justify-center cursor-pointer p-3 hover:bg-gray-200">Rules</label>
-          <input id="rules" className="hidden peer/rules" type="radio" name="nav" />
-          <div className="invisible absolute inset-x-0 bottom-0 top-20 bg-white peer-checked/rules:visible">the content of rules</div>
-        </div>
-
-        <div className="relative w-full h-96">
-          <label htmlFor="prizes" className="flex justify-center cursor-pointer p-3 hover:bg-gray-200">Prizes</label>
-          <input id="prizes" className="hidden peer/prizes" type="radio" name="nav" />
-          <div className="invisible absolute inset-x-0 bottom-0 top-20 bg-white peer-checked/prizes:visible">the content of prizes</div>
-        </div>
-      </div>
-
       <hr />
     </div >
   );
