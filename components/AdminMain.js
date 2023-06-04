a7fed77eaed2905807f8b8a4563f61bb106df2ba
Cereno Deng <cereno.deng@outlook.com>
2023-06-04 23:47:47 +0800

    add admin header

--- components/AdminMain.js
+++ components/AdminMain.js
@@ -1,7 +1,10 @@
+import AdminHeader from "./AdminHeader";
+
 export default async function AdminMain({ children }) {
 
   return (
-    <main className="flex flex-col p-10 w-4/5">
+    <main className="flex flex-col gap-y-10 p-10 w-4/5">
+      <AdminHeader title="Welcome to Admin" />
       {children}
     </main>
   );
