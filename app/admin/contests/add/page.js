c5b6353452fcbb22cd4478fbd2d54bbd1630ca25
Cereno Deng <cereno.deng@outlook.com>
2023-06-05 23:52:29 +0800

    add admin contest adding page

---
+++ app/admin/contests/add/page.js
@@ -0,0 +1,8 @@
+export default async function ContestsAdd() {
+
+  return (
+    <div>
+      contests add
+    </div>
+  );
+}
--- components/AdminHeader.js
+++ components/AdminHeader.js
@@ -1,22 +1,27 @@
 'use client';
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default async function AdminHeader() {
   const pathname = usePathname();
   const title = pathname.split('/').at(-1);
 
+  const firstUpperCase = (text) => {
+    return text[0].toUpperCase() + text.slice(1)
+  };
+
   return (
     <div className="flex justify-between">
-      <h1 className="text-3xl font-bold">{title[0].toUpperCase() + title.slice(1)}</h1>
+      <h1 className="text-3xl font-bold">{title == 'add' ? `Add ${firstUpperCase(pathname.split('/').at(-2)).slice(0, -1)}` : firstUpperCase(title)}</h1>
 
-      <div className="flex gap-x-5">
-        <button type="button" className="flex gap-x-1 text-white bg-indigo-900 px-6 py-3 items-center border-2 border-transparent rounded hover:text-indigo-900 hover:bg-white hover:border-2 hover:border-indigo-900">
+      <div className={`${title == 'add' ? 'hidden' : 'flex'} gap-x-5`}>
+        <Link href={`/admin/${title}/add`} className="flex gap-x-1 text-white bg-indigo-900 px-6 py-3 items-center border-2 border-transparent rounded hover:text-indigo-900 hover:bg-white hover:border-2 hover:border-indigo-900">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
           </svg>
           <span className="text-base font-medium">Add</span>
-        </button>
+        </Link>
         <button type="button">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
@@ -24,7 +29,6 @@ export default async function AdminHeader() {
           </svg>
         </button>
       </div>
-
-    </div>
+    </div >
   )
 }
