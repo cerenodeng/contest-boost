9213a209f1e3524c258d24ffefd39cb62e10f638
Cereno Deng <cereno.deng@outlook.com>
2023-06-09 01:51:26 +0800

    add admin contest list

--- app/admin/contests/page.js
+++ app/admin/contests/page.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+import { prisma } from '@/database/init';
+
 export default async function ContestsPage() {
+  const contests = await prisma.contest.findMany();
 
   return (
-    <div>
-      contests list
+    <div className="flex flex-col divide-y">
+      {contests.map((contest) => (
+        <div className="flex py-5" key={contest.id}>
+          <Link href={`/admin/contests/${contest.id}`} className="w-1/4">{contest.title}</Link>
+          <div className="w-1/4">{contest.startDatetime}</div>
+          <div className="w-1/4">{contest.endDatetime}</div>
+          <div className="w-1/4">{JSON.parse(contest.content).goals}</div>
+        </div>
+      ))}
     </div>
   );
 }
