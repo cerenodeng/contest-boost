ed67e78673b03c4b7afa65043eb4edcade45d7dd
Cereno Deng <cereno.deng@outlook.com>
2023-06-12 02:14:34 +0800

    add admin contest settings

---
+++ app/admin/contests/settings/page.js
@@ -0,0 +1,35 @@
+import { prisma } from '@/database/init';
+import { redirect } from 'next/navigation';
+
+async function Save(data) {
+  'use server';
+
+  const title = data.get('title');
+
+  const contestContentType = await prisma.contestContentType.create({
+    data: {
+      title: title,
+    }
+  });
+
+  console.log(contestContentType);
+  redirect('/admin/contests');
+}
+
+export default async function ContestsSettings() {
+
+  return (
+    <form className="flex flex-col">
+      <div className="flex gap-x-10">
+        <label htmlFor="title" className="label-no-pb">Content Types</label>
+        <div className="w-4/5">
+          <input id="title" name="title" type="text" className="input-no-pb" />
+        </div>
+      </div>
+
+      <div className="flex mt-10 justify-end">
+        <button formAction={Save} className="button-primary">Save</button>
+      </div>
+    </form>
+  );
+}
