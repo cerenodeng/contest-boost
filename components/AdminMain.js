15f311897c188761593030a16da9d98625bfb4e4
Cereno Deng <cereno.deng@outlook.com>
2023-06-13 01:22:38 +0800

    add admin contest searching

--- components/AdminMain.js
+++ components/AdminMain.js
@@ -1,10 +1,12 @@
 import AdminHeader from "./AdminHeader";
+import AdminSearch from "./AdminSearch";
 
 export default async function AdminMain({ children }) {
 
   return (
     <main className="flex flex-col gap-y-10 p-10 w-4/5">
       <AdminHeader />
+      <AdminSearch />
       {children}
     </main>
   );
---
+++ components/AdminSearch.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default async function AdminSidebar() {
+  return (
+    <div class="w-1/5">
+      <input name="search" type="text" className="input" placeholder="Search..." />
+    </div>
+  )
+}
