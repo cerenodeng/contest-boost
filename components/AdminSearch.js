0b243cf207b4843b567ad7520f9555378d5f56a1
Cereno Deng <cereno.deng@outlook.com>
2023-06-14 15:47:27 +0800

    fix admin searching location

--- components/AdminSearch.js
+++ components/AdminSearch.js
@@ -1,8 +1,16 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
+
 export default async function AdminSidebar() {
+  const sidebarNavs = ['contests', 'pages', 'users'];
+  const pathname = usePathname();
+  const lastSegment = pathname.split('/').at(-1);
+
+  const searchClass = sidebarNavs.includes(lastSegment) ? '' : 'hidden';
+
   return (
-    <div class="w-1/5">
+    <div className={`w-1/5 ${searchClass}`}>
       <input name="search" type="text" className="input" placeholder="Search..." />
     </div>
   )
