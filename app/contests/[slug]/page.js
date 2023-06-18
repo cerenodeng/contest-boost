c613b715b2f78318a638acfa57882cf081ed2c80
Cereno Deng <cereno.deng@outlook.com>
2023-06-18 18:32:53 +0800

    change data structure for nav tabs

--- app/contests/[slug]/page.js
+++ app/contests/[slug]/page.js
@@ -2,12 +2,22 @@ import { prisma } from '@/database/init';
 import AnimatedTabs from '@/components/AnimatedTabs';
 
 export default async function ContestPage({ params }) {
-  const navTabs = await prisma.navTab.findMany();
+  const contest = await prisma.contest.findUnique({
+    where: {
+      id: Number(params.slug),
+    },
+  });
+  const content = JSON.parse(contest.content);
+
+  const tabs = [];
+  for (let key in content) {
+    tabs.push({ label: key[0].toUpperCase() + key.slice(1), content: content[key] });
+  }
 
   return (
     <main className="flex p-20 h-screen justify-center">
       <div className="flex flex-col gap-y-10 w-full items-center">
-        <AnimatedTabs tabs={navTabs} />
+        <AnimatedTabs tabs={tabs} />
 
         <hr />
       </div >
