5e9945428f0ce17c1968317fa9fbe2ba176d23e1
Cereno Deng <cereno.deng@outlook.com>
2023-06-25 22:31:49 +0800

    change root page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Link from "next/link";
 
-export default function Home() {
+export default function RootPage() {
   return (
-    <main className="flex flex-col min-h-screen min-w-full items-center justify-center p-24">
-      Course Boost
+    <main className="flex flex-col gap-y-10 min-h-screen min-w-full items-center justify-center p-24">
+      <h1>Contest Boost</h1>
+      <Link href="/admin">Login</Link>
     </main>
   )
 }
